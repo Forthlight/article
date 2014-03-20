78d4d14785736c215b07a4ef0b9986e96ebc40c9
vagrant <alx.l@gmx.com>
2014-03-20 08:38:12 +0000

    Updated engine

--- lib/article/engine.rb
+++ lib/article/engine.rb
@@ -1,5 +1,11 @@
 module Article
   class Engine < ::Rails::Engine
     isolate_namespace Article
+
+    # Generators
+    config.generators do |g|
+       g.test_framework :rspec, :fixture => true, :views => false
+       g.fixture_replacement :fabrication
+    end
   end
 end
