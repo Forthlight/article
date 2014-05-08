6892c93c320216ad8ce1cdaa919d574d9d1514f5
alxl <alx.l@gmx.com>
2014-05-08 20:21:42 +0000

    General bugfixing and cleanup

--- app/models/article/type.rb
+++ app/models/article/type.rb
@@ -12,7 +12,7 @@ module Article
     field :description, type: String
 
     validates :title, presence: true, length: { maximum: 20 }, uniqueness: {case_sensitive: false}, format: { with: /\A[a-zA-Z0-9_\s,\.&]+\Z/ }
-    validates :description, presence: true, length: { maximum: 255 }, format: { with: /\A[a-zA-Z0-9\s]+\Z/ }
+    validates :description, presence: true, length: { maximum: 255 }
 
     def sanitize_title
       self.title = self.title.gsub(/\s+/, "_")
