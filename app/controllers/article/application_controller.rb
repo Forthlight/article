41ec5fe25c9c3e972c9f6ea322bdc2c0de22e39b
alxl <alx.l@gmx.com>
2014-05-27 11:47:03 +0000

    Added common domain helpers

--- app/controllers/article/application_controller.rb
+++ app/controllers/article/application_controller.rb
@@ -1,5 +1,6 @@
 module Article
   class Article::ApplicationController < ::ApplicationController
-
+    # Include helpers from common domain engine
+    helper CommonDomain::Engine.helpers
   end
 end
